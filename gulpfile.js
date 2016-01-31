7aee55fefe94cb2f32f76c0ab1d70af5f224bf7e
kenyakodaira <kenyanke.1@gmail.com>
2016-01-31 14:52:13 +0900

    Fix README

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,14 @@ var runSequence = require("run-sequence");
 var extender = require("gulp-html-extend");
 
 // clean
+// ビルドする前のファイルを削除する
 gulp.task('clean', function (cb) {
   var rimraf = require('rimraf');
   rimraf('build/**', cb);
 });
 
 // server
+// ローカルサーバーを起動
 gulp.task("server", function() {
   browser({
     server: {
@@ -24,54 +26,66 @@ gulp.task("server", function() {
 });
 
 // extend
+// HTMLファイルのテンプレート化
 gulp.task("extend", function() {
   gulp.src(["dev/**/*.html", "!dev/include/**/*.html"])
   .pipe(plumber())
   .pipe(extender({annotations:false,verbose:false}))
-  .pipe(gulp.dest('build'));
+  .pipe(gulp.dest('build'))
+  .pipe(browser.reload({stream:true}));
 });
 
 // babel
+// es6をjsファイルに変換
 gulp.task("babel", function() {
   gulp.src("dev/js/**/*.es6")
   .pipe(plumber())
   .pipe(babel())
   .pipe(uglify())
-  .pipe(gulp.dest("build/js"));
+  .pipe(gulp.dest("build/js"))
+  .pipe(browser.reload({stream:true}));
 });
 
-// sass & autoprefix & glup-frontnote
+// sass
+// sassファイルをcssに変換かつ圧縮
 gulp.task("sass", function() {
   gulp.src("dev/sass/**/*.scss")
   .pipe(plumber())
   .pipe(sass())
   .pipe(minifyCss({compatibility: 'ie8'}))
-  .pipe(gulp.dest("build/css"));
+  .pipe(gulp.dest("build/css"))
+  .pipe(browser.reload({stream:true}));
 });
 
 // js
+// jsファイルを圧縮
 gulp.task("js", function() {
   gulp.src("dev/js/**/*.js")
   .pipe(plumber())
   .pipe(uglify())
-  .pipe(gulp.dest("build/js"));
+  .pipe(gulp.dest("build/js"))
+  .pipe(browser.reload({stream:true}));
 });
 
 // copy
+// htmlファイルをコピー
 gulp.task("copy_html", function() {
   gulp.src("dev/*.html")
   .pipe(plumber())
-  .pipe(gulp.dest("build"));
+  .pipe(gulp.dest("build"))
+  .pipe(browser.reload({stream:true}));
 });
-
+// images/以下をコピー
 gulp.task("copy_img", function() {
   gulp.src("dev/images/**")
   .pipe(plumber())
-  .pipe(gulp.dest("build/images"));
+  .pipe(gulp.dest("build/images"))
+  .pipe(browser.reload({stream:true}));
 });
 
 
 // watch
+// ファイルを監視
 gulp.task("watch", function() {
   gulp.watch("dev/**/*.html", ["extend"]);
   gulp.watch("dev/*.html", ["copy_html"]);
@@ -83,6 +97,7 @@ gulp.task("watch", function() {
 
 
 // gulp task
+// 「gulp」コマンドによる処理を直列処理で実行
 gulp.task("default", function() {
   runSequence("clean", ["extend", "sass", "babel", "js", "copy_html", "copy_img"], "watch", "server");
 });
